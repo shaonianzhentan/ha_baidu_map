6fa487880f6d4d6e7437f80e9f1eb2467301e5ba
zhulu <shaonianzhentan@sina.cn>
2020-01-09 18:21:25 +0800

    准备搞GPS轨迹

--- custom_components/ha_baidu_map/__init__.py
+++ custom_components/ha_baidu_map/__init__.py
@@ -1,8 +1,11 @@
 import os
 import uuid
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'ha_baidu_map'
-VERSION = '1.0'
+VERSION = '1.0.1'
 URL = '/ha_baidu_map-api-'+ str(uuid.uuid4())
 ROOT_PATH = URL + '/' + VERSION
 
@@ -19,6 +22,17 @@ def setup(hass, config):
     _icon = cfg.get('icon', 'mdi:map-marker-radius')
     _ak = config.get("ak", 'ha_cloud_music')
 
+    _LOGGER.info('''
+-------------------------------------------------------------------
+
+    百度地图【作者QQ：635147515】
+    版本：''' + VERSION + '''    
+    项目地址：https://github.com/shaonianzhentan/ha_baidu_map
+    安装信息：
+        通信地址：''' + URL + '''
+    
+-------------------------------------------------------------------''')
+
     hass.components.frontend.async_register_built_in_panel(
         "baidu-map",
         _name,
@@ -27,4 +41,32 @@ def setup(hass, config):
         config={"ak": _ak},
         require_admin=True)
 
+    hass.http.register_view(HassGateView)
     return True
+
+class HassGateView(HomeAssistantView):
+
+    url = URL
+    name = DOMAIN
+    requires_auth = False
+    
+    async def post(self, request):
+        hass = request.app["hass"]
+        try:
+            reader = await request.multipart()
+            file = await reader.next()
+            # 生成文件
+            filename = os.path.dirname(__file__) + '/' + str(uuid.uuid4())+ '.json'
+            size = 0
+            with open(filename, 'wb') as f:
+                while True:
+                    chunk = await file.read_chunk()  # 默认是8192个字节。
+                    if not chunk:
+                        break
+                    size += len(chunk)
+                    f.write(chunk)
+        
+            return self.json({'code':1, 'msg': '百度语音识别错误'})            
+        except Exception as e:
+            print(e)
+            return self.json({'code':1, 'msg': '出现异常'})
