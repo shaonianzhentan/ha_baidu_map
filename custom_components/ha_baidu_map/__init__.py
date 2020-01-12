36b42ccd1c614f221b25502f777aef3b973b45f8
zhulu <shaonianzhentan@sina.cn>
2020-01-12 18:37:30 +0800

    up

--- custom_components/ha_baidu_map/__init__.py
+++ custom_components/ha_baidu_map/__init__.py
@@ -1,6 +1,8 @@
 import os
+import re
 import uuid
 import logging
+import json
 from homeassistant.components.http import HomeAssistantView
 import sqlalchemy
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -10,21 +12,28 @@ _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'ha_baidu_map'
 VERSION = '1.0.2'
-URL = '/ha_baidu_map-api-'+ str(uuid.uuid4())
+URL = '/ha_baidu_map-api'
 ROOT_PATH = URL + '/' + VERSION
+API_KEY = str(uuid.uuid4())
 
 def setup(hass, config):
     cfg  = config[DOMAIN]
+    _name = cfg.get('name', '百度地图')
+    _icon = cfg.get('icon', 'mdi:map-marker-radius')
+    _ak = cfg.get("ak", 'ha_cloud_music')
+     # 验证api_key是否设置为uuid格式（为了安全，必须要这么做）
+    _api_key = cfg.get('api_key', '')
+    global API_KEY
+    if _api_key != '' and re.match('\w{8}(-\w{4}){3}-\w{12}', _api_key):
+        _LOGGER.info('【百度地图】使用自定义api_key：' + _api_key)
+        API_KEY = _api_key
+    
     # 注册静态目录
     local = hass.config.path("custom_components/ha_baidu_map/local")
     if os.path.isdir(local):
         hass.http.register_static_path(ROOT_PATH, local, False)
 
     hass.components.frontend.add_extra_js_url(hass, ROOT_PATH + '/ha-panel-baidu-map.js')
-
-    _name = cfg.get('name', '百度地图')
-    _icon = cfg.get('icon', 'mdi:map-marker-radius')
-    _ak = config.get("ak", 'ha_cloud_music')
 
     _LOGGER.info('''
 -------------------------------------------------------------------
@@ -33,7 +42,10 @@ def setup(hass, config):
     版本：''' + VERSION + '''    
     项目地址：https://github.com/shaonianzhentan/ha_baidu_map
     安装信息：
-        通信地址：''' + hass.config.api.base_url + URL + '''
+    
+        通信URL：''' + hass.config.api.base_url + URL + '''
+        
+        通信密钥：''' + API_KEY + '''
     
 -------------------------------------------------------------------''')
 
@@ -62,8 +74,18 @@ class HassGateView(HomeAssistantView):
     async def post(self, request):
         hass = request.app["hass"]
         try:
-            res = await request.json()
-            if 'latitude' not in res and 'longitude' not in res and 'device' not in res:
+            res = await request.json()  
+            _LOGGER.info(res)            
+            if 'api_key' not in res:
+                _LOGGER.info('密钥不存在')
+                return self.json({'code':1, 'msg': '密钥不存在'})
+            
+            if res['api_key'] != API_KEY:
+                _LOGGER.info('密钥错误')
+                return self.json({'code':1, 'msg': '密钥错误'})
+
+            if 'latitude' not in res and 'latitude' not in res and 'longitude' not in res and 'device' not in res:
+                _LOGGER.info('参数不对')
                 return self.json({'code':1, 'msg': '参数不对'})
                 
             if 'accuracy' not in res:
@@ -80,9 +102,14 @@ class HassGateView(HomeAssistantView):
                 res['activity'] = ''
             if 'provider' not in res:
                 res['provider'] = ''
-
+            if 'starttimestamp' not in res:
+                res['starttimestamp'] = ''
+            if 'dist' not in res:
+                res['dist'] = ''
+                
             sql = hass.data[URL]
             sql.add(res)
+
             '''
             sql.add({
                 'latitude':126.123123123,
@@ -96,8 +123,8 @@ class HassGateView(HomeAssistantView):
                 'provider':'供应商',
                 'activity':'活动'
             })
-            '''            
-            return self.json({'code':0, 'msg': '接收成功'})            
+            '''
+            return self.json(res)
         except Exception as e:
             print(e)
             return self.json({'code':1, 'msg': '出现异常'})
@@ -133,6 +160,8 @@ class SqlLite():
                     altitude text,
                     provider text,
                     activity text,
+                    starttimestamp int,
+                    dist text,
                     cdate datetime default (datetime('now', 'localtime'))
                 )
             ''')
@@ -182,10 +211,11 @@ class SqlLite():
             
     def add(self, gps_info):
         self.execute("""
-        insert into ha_baidu_map(latitude,longitude,device,accuracy,battery,speed,direction,altitude,provider,activity) values('""" 
+        insert into ha_baidu_map(latitude,longitude,device,accuracy,battery,speed,direction,altitude,provider,activity,starttimestamp,dist) values('""" 
         + self.filter(gps_info['latitude']) + """','""" + self.filter(gps_info['longitude']) + """','""" 
         + self.filter(gps_info['device']) + """','""" + self.filter(gps_info['accuracy']) + """','""" 
         + self.filter(gps_info['battery']) + """','""" + self.filter(gps_info['speed']) + """','""" 
         + self.filter(gps_info['direction']) + """','""" + self.filter(gps_info['altitude']) + """','""" 
-        + self.filter(gps_info['provider']) + """','""" + self.filter(gps_info['activity']) + """')
+        + self.filter(gps_info['provider']) + """','""" + self.filter(gps_info['activity']) + """','""" 
+        + self.filter(gps_info['starttimestamp']) + """','""" + self.filter(gps_info['dist']) + """')
         """, lambda sess: sess.commit())
